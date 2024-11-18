0f67f2e47e9308bd470582b132f06688ed2d1297
Joan Gladbach <jjgr29@umsystem.edu>
2024-11-18 12:01:15 -0600

    Add project files.

---
+++ CS201R-Pgm1/CS201R-Pgm1.cpp
@@ -0,0 +1,17 @@
+// CS201R-Pgm1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+	int x1, x2, y1, y2;
+	cout << "Enter the values for x1 & y1:  ";
+	cin >> x1 >> y1;
+	cout << "Enter the values for x2 & y2:  ";
+	cin >> x2 >> y2;
+
+	cout << "The distance between the points is: " << endl;
+}
+ 
